80100bf3fa7c1148fd7508c5b363a8c0464aeaa7
tanveershuvo <tanveershuvos@gmail.com>
2020-11-26 23:19:31 +0600

    DB added

--- admin/loginsave.php
+++ admin/loginsave.php
@@ -18,3 +18,20 @@ if (isset($_POST["submit"])) {
     $_SESSION['msg'] = 'Login invalid';
   }
 }
+
+if (isset($_POST["submit"])) {
+  $email = mysqli_real_escape_string($conn, $_POST['email']);
+  $password = mysqli_real_escape_string($conn, md5($_POST['password']));
+  $sql = "SELECT * FROM users where email ='$email' AND password='$password' AND role = 1";
+  $result = $conn->query($sql);
+  $row = mysqli_fetch_assoc($result);
+  if ($result->num_rows > 0 && $row['role'] == 1) {
+    $_SESSION['isLoggedIn'] = TRUE;
+    $_SESSION['email'] = $row['email'];
+    $_SESSION['name'] = $row['name'];
+    header('Location:dashboard');
+  } else {
+    header('Location:index');
+    $_SESSION['msg'] = 'Login invalid';
+  }
+}
