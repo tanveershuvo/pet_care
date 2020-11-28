5d46eb7aea01257eca63fc9d09ac1b13813b6de0
unknown <nissan19rocks@gmail.com>
2020-11-28 13:17:32 +0600

    [title] - adoption issue

--- adoption.php
+++ adoption.php
@@ -3,7 +3,7 @@
 <?php
 include_once("dbConnection/dbCon.php");
 $conn = connect();
-$sql = 'SELECT * FROM adoptionPost';
+$sql = 'SELECT adoptionpost.*,location.location FROM `adoptionpost`,location WHERE adoptionpost.location_id = location.id ';
 $result = $conn->query($sql);
 ?>
 
@@ -14,7 +14,26 @@ $result = $conn->query($sql);
                 <div class="panel panel-default">
                     <div class="panel-heading">Search Section</div>
                     <div class="panel-body">
+                        <form action="">
+                            <div class="form-group row">
+                                <div class="col-sm-8">
+                                    <select class="form-control" name="location">
+                                        <option value="">Select Location</option>
+                                        <?php
+                                        $sql_location = "SELECT * FROM `location`";
+                                        $result_location = $conn->query($sql_location);
+                                        foreach ($result_location as $key => $row) {
+                                        ?>
+                                            <option value="<?= $row['id'] ?>"><?= $row['location'] ?></option>
+                                        <?php } ?>
 
+                                    </select>
+                                </div>
+                                <div class="col-sm-4">
+                                    <a type="submit" class="btn btn-warning"><i class="fa fa-search"></i></a>
+                                </div>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
@@ -32,6 +51,7 @@ $result = $conn->query($sql);
                                         <div class="media-body">
                                             <h4 class="media-heading"><?= $value['title'] ?></h4>
                                             <p>Description: <?= $value['description'] ?></p>
+                                            <p>Location: <?= $value['location'] ?></p>
                                             <a class="btn btn-info btn-block btn-sm" href="adoption_details.php?id=<?= $value['id']; ?>">View all Details</a>
                                         </div>
                                     </div>
